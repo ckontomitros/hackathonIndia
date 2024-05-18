111b3ed28e373b321bc09c37371a7a976452792d
christos.kontomitros <christos.kontomitros@telekom.com>
2024-05-18 13:11:05 +0530

    add cors

--- src/main/java/com/dt/pausepick/controllers/ActorController.java
+++ src/main/java/com/dt/pausepick/controllers/ActorController.java
@@ -3,6 +3,7 @@ package com.dt.pausepick.controllers;
 import com.dt.pausepick.services.ActorRecognitionService;
 import com.dt.pausepick.services.WorkItem;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/actor")
+@CrossOrigin
 public class ActorController {
     private final ActorRecognitionService actorRecognitionService;
 
