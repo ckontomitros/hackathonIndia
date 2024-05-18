407a563f34e6e956321111821b1eaad694dec1cf
christos.kontomitros <christos.kontomitros@telekom.com>
2024-05-18 11:08:59 +0530

    first commit

---
+++ src/main/java/com/dt/pausepick/PausepickApplication.java
@@ -0,0 +1,13 @@
+package com.dt.pausepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PausepickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PausepickApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dt/pausepick/services/RecognizeCelebritiesResult.java
@@ -0,0 +1,4 @@
+package com.dt.pausepick.services;
+
+public class RecognizeCelebritiesResult {
+}
