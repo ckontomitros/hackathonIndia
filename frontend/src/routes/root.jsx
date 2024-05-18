cca6eedaca1312e2159bb86191915c22c5a19ffa
Dimitris Trechas <trechas.dimitrios@telekom.com>
2024-05-18 15:01:19 +0530

    wip

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -30,17 +30,38 @@ const Root = () => {
     }
   };
 
-  useEffect(() => {
-    console.log("isLoading", isLoading);
-  }, [isLoading]);
+  const showSidebar = isLoading || !!actorNames.length;
+  const mainClassnames = showSidebar
+    ? "my-2 px-2 w-full overflow-hidden xl:w-2/3 "
+    : "my-2 px-2 w-full overflow-hidden";
 
   return (
     <>
       <Header />
-      <main>
-        <video ref={videoRef} src="video.mp4" onPause={handlePause} controls></video>
-        <canvas ref={canvasRef} style={{ display: "" }}></canvas>
-      </main>
+      <div className="container mx-auto">
+        <div className="flex flex-col flex-wrap  overflow-hidden xl:flex-row">
+          <main className={mainClassnames}>
+            <video ref={videoRef} src="video.mp4" onPause={handlePause} controls></video>
+            <canvas ref={canvasRef} style={{ display: "none" }}></canvas>
+          </main>
+          {showSidebar && (
+            <aside className="my-2 px-2 w-full overflow-hidden xl:w-1/3">
+              <h5 className="mb-2 text-lg font-semibold text-gray-900">Actors</h5>
+              {isLoading ? (
+                <div>Searching...</div>
+              ) : !actorNames.length ? (
+                <div>No actors identified.</div>
+              ) : (
+                <ul className="max-w-xl space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+                  {actorNames.map((name, index) => (
+                    <li key={index}>{name}</li>
+                  ))}
+                </ul>
+              )}
+            </aside>
+          )}
+        </div>
+      </div>
     </>
   );
 };
